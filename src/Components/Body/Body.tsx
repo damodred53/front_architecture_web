10cc812daba874a055a867fdcc20a3a1a61a33bc
damodred53 <florent.guyard@live.fr>
2025-09-09 16:58:46 +0200

    arrangement css barre de recherche (pas fini)

--- src/Components/Body/Body.tsx
+++ src/Components/Body/Body.tsx
@@ -1,9 +1,10 @@
 import SearchBar from "../SearchBar/SearchBar";
+import './body.css';
 
 
 const Body = () => {
     return (
-        <div>   
+        <div className="body">   
             <h1>Research Page</h1>
                 <SearchBar />
         </div>
